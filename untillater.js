53d4b5639eb0c307073201fa8f3982ddd335e75c
Ethan Smith <ethan@onebytegone.com>
2015-08-12 21:21:08 -0400

    Added untillater.js

---
+++ untillater.js
@@ -0,0 +1,36 @@
+//  untillater.js 0.0.1
+//  (c) 2015 Ethan Smith
+//  UntilLater may be freely distributed under the MIT license.
+
+(function() {
+   var root = this,
+       UntilLater;
+
+   /**
+    * Creates an instance of UntilLater
+    *
+    * @param date Date After which to call `action`
+    * @param action function
+    * @param element string Selector for the element to fill
+    */
+   UntilLater = function(date, action, element) {
+      if (element === undefined) {
+         element = '#jsLater';
+      }
+   };
+
+
+   /**
+    * Exports to window as needed
+    */
+   if (typeof exports !== 'undefined') {
+      // Support for browserify
+      if (typeof module !== 'undefined' && module.exports) {
+         exports = module.exports = UntilLater;
+      }
+      exports.UntilLater = UntilLater;
+   } else {
+      // Export UntilLater to default scope
+      root.UntilLater = UntilLater;
+   }
+}());
